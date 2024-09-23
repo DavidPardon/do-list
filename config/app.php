75c6b8227f7ffb0b9c17b37c48c2f548fefe19b1
David Alvarado <126503282+DavidPardon@users.noreply.github.com>
2024-09-23 23:07:54 +0000

    nube

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://redesigned-yodel-44xj76jrv7qh79g6-8000.app.github.dev'),
 
     /*
     |--------------------------------------------------------------------------
